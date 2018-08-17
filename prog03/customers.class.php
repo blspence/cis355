a881fe20df9d95d3947a13055e22c7789daad8e0
blspence <blspence@svsu.edu>
2018-08-17 18:19:09 -0400

    update wip

--- prog03/customers.class.php
+++ prog03/customers.class.php
@@ -26,15 +26,15 @@ class Customers
         echo '    <div class="container">                                   ';
         echo '        <div class="span10 offset1">                          ';
         echo '            <div class="row">                                 ';
-        echo '                <h3>Create a Customer</h3>                    ';
+        echo "                <h3>Create a $this->title</h3>                ";
         echo '            </div>                                            ';
-        echo '            <form class="form-horizontal"                      ';
+        echo '            <form class="form-horizontal"                     ';
         echo '                  action="index.php?fnc=id_DB_MOD_CREATE"     ';
         echo '                  method="post">                              ';
 
-        $this->control_group("name", $this->nameError, $this->name);
-        $this->control_group("email", $this->emailError, $this->email);
-        $this->control_group("mobile", $this->mobileError, $this->mobile);
+        $this->control_group("name", 'Name', $this->nameError, $this->name);
+        $this->control_group("email", 'Email Address', $this->emailError, $this->email);
+        $this->control_group("mobile", 'Mobile Number', $this->mobileError, $this->mobile);
 
         echo '                <div class="form-actions">                    ';
         echo '                  <button type="submit"                       ';
@@ -42,7 +42,7 @@ class Customers
         echo '                              Create</button>                 ';
         echo '                    <a class="btn" href="index.php">Back</a>  ';
         echo '                </div>                                        ';
-        echo '            </form>                                            ';
+        echo '            </form>                                           ';
         echo '        </div>                                                ';
         echo '    </div>                                                    ';
         echo '</body>                                                       ';
@@ -121,7 +121,7 @@ class Customers
         echo '    <div class="container">                                   ';
         echo '        <div class="span10 offset1">                          ';
         echo '            <div class="row">                                 ';
-        echo '                <h3>Read a Customer</h3>                      ';
+        echo "                <h3>Read a $this->title</h3>                  ";
         echo '            </div>                                            ';
         echo '            <div class="form-horizontal">                     ';
 
@@ -141,43 +141,40 @@ class Customers
 
     function form_update()
     {
-        echo "
-        <html>
-            <head>
-                <title>Update a $this->title</title>
-                    ";
-        echo "
-                <meta charset='UTF-8'>
-                <link href='https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css' rel='stylesheet'>
-                <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js'></script>
-                    ";
-        echo "
-            </head>
+        echo 'IN FUNC: form_update() <br/>';
 
-            <body>
-                <div class='container'>
+        echo '<html lang="en">                                              ';
+        echo '<head>                                                        ';
+        echo '    <meta charset="utf-8">                                    ';
+        echo '    <link href="../../css/bootstrap.min.css" rel="stylesheet">';
+        echo '    <link href="../../css/cis355.css" rel="stylesheet">       ';
+        echo '</head>                                                       ';
+        echo '                                                              ';
+        echo '<body>                                                        ';
+        echo '    <div class="container">                                   ';
+        echo '        <div class="span10 offset1">                          ';
+        echo '            <div class="row">                                 ';
+        echo "                <h3>Update a $this->title</h3>                ";
+        echo '            </div>                                            ';
+        echo '            <form class="form-horizontal"                     ';
+        echo '                  action="index.php?fnc=id_DB_MOD_UPDATE"     ';
+        echo '                  method="post">                              ';
 
-                    <div class='span10 offset1'>
-                        <p class='row'>
-                            <h3>Update a $this->title</h3>
-                        </p>
-                        <form class='form-horizontal' action='index.php?fnc=id_DB_MOD_UPDATE' method='post'>
-                    ";
-        $this->control_group("name", $this->nameError, $this->name);
-        $this->control_group("email", $this->emailError, $this->email);
-        $this->control_group("mobile", $this->mobileError, $this->mobile);
-        echo "
-                            <div class='form-actions'>
-                                <button type='submit' class='btn btn-success'>Update</button>
-                                <a class='btn' href='index.php'>Back</a>
-                            </div>
-                        </form>
-                    </div>
+        $this->control_group("name", 'Name', $this->nameError, $this->name);
+        $this->control_group("email", 'Email Address', $this->emailError, $this->email);
+        $this->control_group("mobile", 'Mobile Number', $this->mobileError, $this->mobile);
 
-                </div> <!-- /container -->
-            </body>
-        </html>
-                    ";
+        echo '                <div class="form-actions">                    ';
+        echo '                  <button type="submit"                       ';
+        echo '                          class="btn btn-success">            ';
+        echo '                              Update</button>                 ';
+        echo '                    <a class="btn" href="index.php">Back</a>  ';
+        echo '                </div>                                        ';
+        echo '            </form>                                           ';
+        echo '        </div>                                                ';
+        echo '    </div>                                                    ';
+        echo '</body>                                                       ';
+        echo '</html>                                                       ';
     }
 
     function list_records()
@@ -251,14 +248,15 @@ class Customers
         echo "</div>";
     }
 
-    function control_group($label, $labelError, $val)
+    function control_group($input, $label, $labelError, $val)
     {
         echo "<div class='control-group";
         echo !empty($labelError) ? 'error' : '';
         echo "'>";
         echo "<label class='control-label'>$label</label>";
         echo "<div class='controls'>";
-        echo "<input name='$label' type='text' placeholder='$label' value='";
+
+        echo "<input name='$input' type='text' placeholder='$label' value='";
         echo !empty($val) ? $val : '';
         echo "'>";
         if(!empty($labelError))
@@ -299,7 +297,7 @@ class Customers
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $sql = "INSERT INTO customers (name,email,mobile) values(?, ?, ?)";
             $q = $pdo->prepare($sql);
-            $q->execute(array($this->name,$this->email,$this->mobile));
+            $q->execute(array($this->name, $this->email, $this->mobile));
             Database::disconnect();
             header("Location: index.php");
         }
@@ -311,43 +309,67 @@ class Customers
 
     function db_mod_update()
     {
-        /* validate input */
+        echo 'IN FUNC: db_mod_update() <br/>';
+
+        $id = null;
+
+        if(!empty($_GET['id']))
+        {
+            $id = $_REQUEST['id'];
+        }
+
+        if(null == $id)
+        {
+            header("Location: index.php");
+        }
+
         $valid = true;
+
         if(empty($this->name))
         {
-            $this->nameError = 'Please enter Name';
+            $nameError = 'Please enter Name';
             $valid = false;
         }
 
         if(empty($this->email))
         {
-            $this->emailError = 'Please enter Email Address';
-            $valid = false;
-        }
-        else if(!filter_var($email,FILTER_VALIDATE_EMAIL))
-        {
-            $this->emailError = 'Please enter a valid Email Address';
+            $emailError = 'Please enter Email Address';
             $valid = false;
         }
 
         if(empty($this->mobile))
         {
-            $this->mobileError = 'Please enter Mobile Number';
+            $mobileError = 'Please enter Mobile Number';
             $valid = false;
         }
 
         if($valid)
         {
+            echo '$valid <br/>';
+
             $pdo = Database::connect();
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $sql = "UPDATE customers  set name = ?, email = ?, mobile =? WHERE id = ?";
             $q = $pdo->prepare($sql);
-            $q->execute(array($this->name,$this->email,$this->mobile, $this->id));
+            $q->execute(array($this->name, $this->email, $this->mobile, $this->id));
             Database::disconnect();
             header("Location: index.php");
         }
         else
         {
+            echo 'NOT $valid <br/>';
+
+            $pdo = Database::connect();
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sql = "SELECT * FROM customers where id = ?";
+            $q = $pdo->prepare($sql);
+            $q->execute(array($id));
+            $data = $q->fetch(PDO::FETCH_ASSOC);
+            $this->name = $data['name'];
+            $this->email = $data['email'];
+            $this->mobile = $data['mobile'];
+            Database::disconnect();
+
             $this->form_update();
         }
     }
